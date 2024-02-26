8f2b4dcfec81a49b375903ec431d69a58e4314b3
durga0702 <durga@sparkouttech.com>
2024-02-26 18:29:25 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'sw-app';
+  public data:any =[];
+  public singleData:any = {};
+  constructor(private http:HttpClient){}
+
+  ngOnInit(): void {
+    this.http.get("https://jsonplaceholder.typicode.com/todos").subscribe((res)=>{
+      this.data = res;
+    });
+    this.http.get("https://jsonplaceholder.typicode.com/todos/1").subscribe((res)=>{
+      this.singleData = res;
+    });
+  }
 }
